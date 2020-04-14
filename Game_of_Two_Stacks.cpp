5b7c6f07f8bf73940ec5b31651171ecfc66b0db4
Anuj Padmawar <35773688+anuj-padmawar@users.noreply.github.com>
2020-04-14 11:47:17 +0530

    Add files via upload

---
+++ Game_of_Two_Stacks.cpp
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the twoStacks function below.
+     */
+    static int twoStacks(int x, int[] a, int[] b) {
+        /*
+         * Write your code here.
+         */
+         int a_index = 0;
+        int b_index = 0;
+        int count = 0;
+        int sum = 0;
+       
+        while (b_index < b.length && sum + b[b_index] <= x) {
+            sum += b[b_index];
+            b_index++;
+        }
+        
+        b_index--;
+        count = b_index + 1;
+ 
+        while (a_index < a.length && b_index < b.length) {
+            sum += a[a_index];
+            if (sum > x) {
+                while (b_index >= 0) {
+                    sum -= b[b_index];
+                    b_index--;
+                    if (sum <= x) break;
+                }
+                
+                if (sum > x && b_index < 0) {
+                    a_index--;
+                    break;
+                }
+            }
+            count = Math.max(a_index + b_index + 2, count);
+            a_index++;
+        }
+ 
+        return count;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int g = Integer.parseInt(scanner.nextLine().trim());
+
+        for (int gItr = 0; gItr < g; gItr++) {
+            String[] nmx = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nmx[0].trim());
+
+            int m = Integer.parseInt(nmx[1].trim());
+
+            int x = Integer.parseInt(nmx[2].trim());
+
+            int[] a = new int[n];
+
+            String[] aItems = scanner.nextLine().split(" ");
+
+            for (int aItr = 0; aItr < n; aItr++) {
+                int aItem = Integer.parseInt(aItems[aItr].trim());
+                a[aItr] = aItem;
+            }
+
+            int[] b = new int[m];
+
+            String[] bItems = scanner.nextLine().split(" ");
+
+            for (int bItr = 0; bItr < m; bItr++) {
+                int bItem = Integer.parseInt(bItems[bItr].trim());
+                b[bItr] = bItem;
+            }
+
+            int result = twoStacks(x, a, b);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+    }
+}
+
