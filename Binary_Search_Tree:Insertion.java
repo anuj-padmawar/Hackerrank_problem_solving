d159d1d0bda54e3260ec622666f9914f2d602a1e
Anuj Padmawar <35773688+anuj-padmawar@users.noreply.github.com>
2020-04-16 10:36:21 +0530

    Add files via upload

---
+++ Binary_Search_Tree:Insertion.java
@@ -0,0 +1,83 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+   
+  	public static void preOrder( Node root ) {
+      
+    	if( root == null)
+        	return;
+      
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+     
+    }
+
+ /* Node is defined as :
+ class Node 
+    int data;
+    Node left;
+    Node right;
+    
+    */
+
+	public static Node insert(Node root,int data) {
+
+        if(root == null) 
+            {
+                root = new Node(data);
+            }
+        else if(root.data > data)
+            {
+                if(root.left == null)
+                {
+                        Node left = new Node(data);
+                        root.left = left;
+                }
+                else
+                {
+                     insert(root.left,data);
+                }       
+            }
+        else 
+            {
+                if(root.right == null)
+                {
+                        Node right = new Node(data);
+                        root.right = right;
+                }
+                else
+                {
+                     insert(root.right,data);
+                }
+            }   
+
+    return root;
+    }
+
+
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+      	preOrder(root);
+    }	
+}
