de7c9d46749f4eeeeb64d184c65ab863fd30752f
Anuj Padmawar <35773688+anuj-padmawar@users.noreply.github.com>
2020-05-04 11:25:37 +0530

    Add files via upload

---
+++ Sequence_Equation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n+1];
+
+    for(int i=1;i<=n;i++)
+    {
+        int k;
+        cin>>k;
+        arr[k]=i;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cout <<arr[arr[i]]<<endl;
+    }
+
+    return 0;
+}
