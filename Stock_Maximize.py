cd8b2c010a6a5fad6aada22eb644fbbc5765fe93
Anuj Padmawar <35773688+anuj-padmawar@users.noreply.github.com>
2020-08-02 10:10:49 +0530

    Add files via upload

---
+++ Stock_Maximize.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'stockmax' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY prices as parameter.
+#
+
+def stockmax(prices):
+    max=0
+    profit=0
+    for i in reversed(range(0,len(prices))):
+        if max < prices[i]:
+            max=prices[i]
+        profit+=max-prices[i]
+
+    return profit
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        prices = list(map(int, input().rstrip().split()))
+
+        result = stockmax(prices)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
