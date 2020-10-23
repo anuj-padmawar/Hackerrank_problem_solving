41069e95ada14738efba28ae9eff352192812c29
Anuj Padmawar <35773688+anuj-padmawar@users.noreply.github.com>
2020-10-23 13:53:41 +0530

    Add files via upload

---
+++ Nimble_Game.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the nimbleGame function below.
+string nimbleGame(vector<int> s,int N) {
+
+    if(N==1) 
+    {
+            return "Second";
+    }
+    long long nimSum=0;
+    for(int i=1;i<N;i++) 
+    {         
+            if(s[i]%2==1) 
+            {
+                nimSum ^= i;
+            }
+        }
+        if(nimSum!=0) 
+        {
+            return "First";
+        }
+         else 
+        {
+            return "Second";
+        }
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string s_temp_temp;
+        getline(cin, s_temp_temp);
+
+        vector<string> s_temp = split_string(s_temp_temp);
+
+        vector<int> s(n);
+
+        for (int i = 0; i < n; i++) {
+            int s_item = stoi(s_temp[i]);
+
+            s[i] = s_item;
+        }
+
+        string result = nimbleGame(s,n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
